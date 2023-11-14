f8356763e1d89ab4442eae23cf801d23a1c5664e
ptcl221202 <huynhduy221202@gmail.com>
2023-11-14 12:17:29 +0700

    update

--- Baptaplon/Screen3.js
+++ Baptaplon/Screen3.js
@@ -5,6 +5,7 @@ import {StyleSheet,
   Image,
   Pressable,
   FlatList,
+  ScrollView,
   Button,
 } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
@@ -14,23 +15,25 @@ var array = [
     {
         image:require('./IMG/iconmattrang.png'),
         name:'Chế độ tối',
-        des: 'show something',
+        show: '>',
         type: 'roadbike',
     },
     {
         image:require('./IMG/trangthai.png'),
         name:'Trạng thái hoạt động',
-        des: 'show something',
+        show: '>',
         type: 'roadbike',
     },
     {
         image:require('./IMG/tronang.png'),
         name:'Trợ năng',
+        show: '>',
         type: 'mountain',
     },
     {
         image:require('./IMG/quyenriengtu.png'),
         name:'Quyền riêng tư & an toàn',
+        show: '>',
         type: 'mountain',
     },
 ]
@@ -38,23 +41,51 @@ var array2 = [
     {
         image:require('./IMG/avataricon.png'),
         name:'Chế độ tối',
-        des: 'show something',
+        show: '>',
         type: 'roadbike',
     },
     {
         image:require('./IMG/thongbao.png'),
         name:'Trạng thái hoạt động',
-        des: 'show something',
+        show: '>',
         type: 'roadbike',
     },
     {
         image:require('./IMG/dondathang.png'),
         name:'Trợ năng',
+        show: '>',
         type: 'mountain',
     },
     {
         image:require('./IMG/a&file.png'),
         name:'Quyền riêng tư & an toàn',
+        show: '>',
+        type: 'mountain',
+    },
+]
+var array3 = [
+    {
+        image:require('./IMG/chuyentk.png'),
+        name:'Chuyển tài khoản',
+        show: '>',
+        type: 'roadbike',
+    },
+    {
+        image:require('./IMG/baocao.png'),
+        name:'Báo cáo sự cố',
+        //show: '>',
+        type: 'roadbike',
+    },
+    {
+        image:require('./IMG/trogiup.png'),
+        name:'Trợ năng',
+        show: '>',
+        type: 'mountain',
+    },
+    {
+        image:require('./IMG/phaply.png'),
+        name:'Pháp lý & chính sách',
+        show: '>',
         type: 'mountain',
     },
 ]
@@ -67,9 +98,11 @@ function Screen3({navigation}){
     var [state, setState] = React.useState(array);
     var [color , setColor] = React.useState(1);
     var [state2, setState] = React.useState(array2);
+    var [state3, setState] = React.useState(array3);
     //const [searchTerm, setSearchTerm] = useState('');
   return(
-    <View style={ { flex:2 ,backgroundColor:'black',width:"100%",justifyContent:"center",alignItems:"center"}}>
+    <ScrollView style={{ flex: 3, backgroundColor: 'black',height:"1080px" }}>
+    <View style={ {backgroundColor:'black',width:"100%",justifyContent:"center",alignItems:"center"}}>
         <View style={{alignItems: "center"}}>
         <View style={{flexDirection:"row"}}>
             <Text style={{color:"white",left:"20px",fontSize:20}}> Cài đặt </Text>
@@ -87,7 +120,10 @@ function Screen3({navigation}){
             }}/>
         <Text style={{color:"white",fontWeight:"700",fontSize:18}}>Martin Radolph</Text>
         </View>
-        <View style={{flex:0.55, backgroundColor:"dimgray", width:"90%",justifyContent:"center",
+
+        <View style={{ width:390,alignItems:"center",height:"1080px"}} >
+
+        <View style={{ backgroundColor:"dimgray", width:"90%",justifyContent:"center",
                         borderRadius:10,
                         }}>
                 <FlatList
@@ -100,40 +136,83 @@ function Screen3({navigation}){
                             //navigation.navigate('Screen3');
                         }}
                         style={{
+                            borderBottomWidth: 1,
                             height: '50px',
                             width: '100%',
                             borderRadius: '10px',
                             margin: '5px',
                             flexDirection:"row",
-                            justifyContent:"flex-start",
+                            justifyContent:"space-between",
                             alignItems:'center',
-                            borderBottomWidth:"1px"
+                           
                             //marginBottom:"30px"
                         }} >
-                    
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                             <Image 
                             source={item.image}
                             style={{width:"35px", height:'35px',borderRadius:20, alignItems:'flex-start'}}
                             //resizeMode='contain'
                             />
                             <Text style={{color:"white",marginLeft:"10px"}}>{item.name}</Text>
-
+                        </View>
+                            <Text style={{fontSize:"20px",marginLeft:"50px"}}>{item.show}</Text>
+                            
                         </Pressable>
                     );
                 }}
                 />
                 </View>
-                <View style={{flex:0.1, backgroundColor:"dimgray", width:"90%",justifyContent:"center",
-                        borderRadius:10,marginTop:"20px"
+                <View style={{ backgroundColor:"dimgray", width:"90%",justifyContent:"flex-start",
+                        borderRadius:10,marginTop:"20px",flexDirection:"row",alignItems:"center"
                         }}>
                 <Image source={require("./IMG/giamsat.png")} style={{width:"35px",height:"35px", borderRadius:"40px"}}/>
+                <Text style={{color:"white",textAlign:"right"}}> Giám sát  </Text>
                 </View>
-                <View style={{flex:0.6, backgroundColor:"dimgray", width:"90%",justifyContent:"center",
+                <View style={{ backgroundColor:"dimgray", width:"90%",justifyContent:"center",
                         borderRadius:10,marginTop:"20px"
                         }}>
                 <FlatList
                 numColumns={1}
                 data={state2}
+                renderItem={({ item }) => {
+                    return(
+                        <Pressable
+                        onPress={() => {
+                            //navigation.navigate('Screen3');
+                        }}
+                        style={{
+                            height: '50px',
+                            width: '100%',
+                            borderRadius: '10px',
+                            margin: '5px',
+                            flexDirection:"row",
+                            justifyContent:"space-between",
+                            alignItems:'center',
+                            borderBottomWidth:"1px"
+                            //marginBottom:"30px"
+                        }} >
+                    
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Image 
+                            source={item.image}
+                            style={{width:"35px", height:'35px',borderRadius:20, alignItems:'flex-start'}}
+                            //resizeMode='contain'
+                            />
+                            <Text style={{color:"white",marginLeft:"10px"}}>{item.name}</Text>
+                        </View>
+                            <Text style={{fontSize:"20px",marginLeft:"50px"}}>{item.show}</Text>
+
+                        </Pressable>
+                    );
+                }}
+                />
+                </View>
+                <View style={{ backgroundColor:"dimgray", width:"90%",justifyContent:"center",
+                        borderRadius:10,marginTop:"20px"
+                        }}>
+                <FlatList
+                numColumns={1}
+                data={state3}
                 renderItem={({ item }) => {
                     return(
                         <Pressable
@@ -164,43 +243,27 @@ function Screen3({navigation}){
                 }}
                 />
                 </View>
-                <View style={{flex:0.6, backgroundColor:"dimgray", width:"90%",justifyContent:"center",
-                        borderRadius:10,marginTop:"20px"
-                        }}>
-                <FlatList
-                numColumns={1}
-                data={state2}
-                renderItem={({ item }) => {
-                    return(
-                        <Pressable
-                        onPress={() => {
-                            //navigation.navigate('Screen3');
-                        }}
-                        style={{
-                            height: '50px',
-                            width: '100%',
-                            borderRadius: '10px',
-                            margin: '5px',
-                            flexDirection:"row",
-                            justifyContent:"flex-start",
-                            alignItems:'center',
-                            borderBottomWidth:"1px"
-                            //marginBottom:"30px"
-                        }} >
-                    
-                            <Image 
-                            source={item.image}
-                            style={{width:"35px", height:'35px',borderRadius:20, alignItems:'flex-start'}}
-                            //resizeMode='contain'
-                            />
-                            <Text style={{color:"white",marginLeft:"10px"}}>{item.name}</Text>
-
-                        </Pressable>
-                    );
-                }}
-                />
+                <View 
+                style={{flex:0.55,
+                    backgroundColor:"dimgray", 
+                    width:"90%",
+                    justifyContent:"center",
+                    borderRadius:10,
+                    marginTop:"20px"
+                    }}>
+                <View style={{flexDirection:"row", alignItems:"center"}}>
+                <Image source={require("./IMG/metaicon.png")} style={{width:"35px", height:'35px',borderRadius:20, alignItems:'flex-start'}}></Image>
+                <Text style={{color:"white", fontSize:"18px"}}>Meta</Text>
                 </View>
-    </View>
+                    <Text style={{color:"white", fontSize:"16px", fontWeight:"600"}}>Trung tâm tài khoản</Text>
+                    <Text style={{color:"white", fontSize:"14px"}}>Quản lý phần cài đặt tài khoản và trải nghiệm kết nối trên các công nghệ của Meta
+                    </Text>
+                    
+                
+                </View>
+            </View>
+            </View>
+    </ScrollView>
   )
 }
 const styles = StyleSheet.create({
