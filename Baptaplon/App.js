cc7394923237fe6d5bf578107050f5c060c64da3
ptcl221202 <huynhduy221202@gmail.com>
2023-11-15 20:10:40 +0700

    Screen2_1

--- Baptaplon/App.js
+++ Baptaplon/App.js
@@ -5,6 +5,7 @@ import Screen1 from '../Baptaplon/Screen1';
 import Screen2 from '../Baptaplon/Screen2'
 import Screen3 from '../Baptaplon/Screen3'
 import Screen3_2 from '../Baptaplon/Screen3_2'
+import Screen2_1 from '../Baptaplon/Screen2_1'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
   return (
    <NavigationContainer>
     <Stack.Navigator>
+    <Stack.Screen name='Screen2_1' component={Screen2_1}/>
     <Stack.Screen name='Screen3_2' component={Screen3_2}/>
     <Stack.Screen name='Screen3' component={Screen3} options={{headerShown:false}}/>
     <Stack.Screen name='Screen1' component={Screen1} options={{headerShown:false}}/>
--- Baptaplon/Screen3.js
+++ Baptaplon/Screen3.js
@@ -133,7 +133,10 @@ function Screen3({navigation}){
                     return(
                         <Pressable
                         onPress={() => {
-                            //navigation.navigate('Screen3');
+                            if(item.name === "Chế độ tối"){
+                                
+                            }
+                            
                         }}
                         style={{
                             borderBottomWidth: 1,
